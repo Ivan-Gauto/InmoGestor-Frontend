8c66ec1014b28df079a1193439e6cb40d3f7e56d
Ivan-Gauto <ivangauto602160@gmail.com>
2026-04-21 13:21:47 -0300

    Correccion de error en registro

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Box, Typography, Card, CardContent, TextField, Button, Avatar, Alert, CircularProgress, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 import HomeWorkIcon from '@mui/icons-material/HomeWorkOutlined';
 import { useNavigate } from 'react-router-dom';
