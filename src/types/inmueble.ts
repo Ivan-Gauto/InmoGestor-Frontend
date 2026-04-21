a653489c0f00a7b7b4a055c203a77b07d3aeeac4
Ivan-Gauto <ivangauto602160@gmail.com>
2026-04-20 22:36:18 -0300

    Se borraron tipos no implementados

--- src/types/inmueble.ts
+++
@@ -1,9 +0,0 @@
-export interface Inmueble {
-  id: number;
-  direccion: string;
-  descripcion: string;
-  disponibilidad: number;
-  disponibilidadNombre: string;
-  propietarioNombreCompleto: string;
-  tipoNombre: string;
-}
--- src/types/inquilino.ts
+++
@@ -1,6 +0,0 @@
-export interface Inquilino {
-  dni: string;
-  nombreCompleto: string;
-  nombre: string;
-  apellido: string;
-}
